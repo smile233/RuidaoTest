92bc7eb9ecb20545665023f71352ffe7e70b59ad
韩喆 <hz@hanzhedemacbook-air.local>
2016-04-09 08:29:51 +0800

    test1

---
+++ OnlineRuidao/OnlineRuidao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnlineRuidao
+//
+//  Created by 韩喆 on 16/4/9.
+//  Copyright © 2016年 neusoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
